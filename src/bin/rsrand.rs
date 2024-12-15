2a061bb7d3dbb72b1ba69f88c3c60a9234597448
Justin Hugon <jhugon@users.noreply.github.com>
2024-12-15 14:37:52 -0600

    rsrand writes to an output file and takes arguments

--- src/bin/rsrand.rs
+++ src/bin/rsrand.rs
@@ -1,13 +1,30 @@
+use clap::Parser;
+use patharg::OutputArg;
+use std::io::Write;
 use rand::prelude::*;
 use rand_distr::StandardNormal;
 
-fn main() {
+/// Generate random numbers
+///
+/// Generates random samples (numbers) and outputs them in text form, one number per line.
+#[derive(Parser)]
+struct Arguments {
+    /// File to write the output to
+    #[arg(short = 'o', long, default_value_t)]
+    outfile: OutputArg,
 
-    let n = 10;
+    /// Number of samples to generate
+    #[arg()]
+    nsamples: u64,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Arguments::parse();
+    let mut outfile = args.outfile.create()?;
     let dist = StandardNormal;
-    println!("Hello, world! {}",n);
-    for _ in 0..n {
+    for _ in 0..args.nsamples {
         let val: f64 = thread_rng().sample(dist);
-        println!("{}",val);
+        writeln!(&mut outfile,"{}",val)?;
     }
+    Ok(())
 }
