804b666f791a59884d6417f5ab62e34bf980b657
Justin Hugon <jhugon@users.noreply.github.com>
2024-12-15 17:35:38 -0600

    rshist: has basic histogram creation

---
+++ src/bin/rshist.rs
@@ -0,0 +1,63 @@
+use clap::Parser;
+use patharg::InputArg;
+
+/// Make a histogram from input data
+///
+/// Makes a histogram from input data, in text form one number per line.
+#[derive(Parser)]
+struct Arguments {
+    /// File to write the output to
+    #[arg(required=true,default_value_t)]
+    infile: InputArg,
+}
+
+fn main() -> std::io::Result<()> {
+    let mut hist = Histogram::new(vec![0.,0.25,0.5,0.75,1.]);
+
+    let args = Arguments::parse();
+    for line in args.infile.lines()? {
+        let s = line?;
+        let val: f32 = s.parse().expect("Couldn't parse float");
+        hist.fill(val);
+    }
+    println!("{:?}",hist);
+    hist.print();
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Histogram {
+    nbins: usize,
+    bincontent: Vec<u64>,
+    binedges: Vec<f32>,
+}
+
+/// A 1D Histogram
+///
+/// Bin edges include lower bound but not upper bound
+impl Histogram {
+    pub fn new(binedges: Vec<f32>) -> Self {
+        let nbins = binedges.len() - 1;
+        let bincontent = vec![0;nbins];
+        Self{nbins,bincontent,binedges}
+    }
+    fn fill(& mut self, x: f32) -> () {
+        if x < self.binedges[0] {
+            return
+        }
+        for i in 0..self.nbins {
+            if x < self.binedges[i+1] {
+                self.bincontent[i] += 1;
+                return
+            }
+        }
+    }
+    fn print(&self) -> () {
+        for i in 0..self.nbins {
+            let count: usize = self.bincontent[i].try_into().unwrap();
+            let bar: String = std::iter::repeat("X").take(count).collect();
+            println!("{:>10} |{}",self.binedges[i],bar);
+        }
+        println!("{:>10}",self.binedges[self.nbins]);
+    }
+}
