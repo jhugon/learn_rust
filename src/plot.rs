18f5b631507198d0d54c02abf77d796681af2899
Justin Hugon <jhugon@users.noreply.github.com>
2025-01-09 15:35:23 -0600

    making plot mor functional--refactoring

--- src/plot.rs
+++ src/plot.rs
@@ -1,71 +1,29 @@
 use std::iter::zip;
 use std::error::Error;
-use crate::plotutils::drawxaxis;
+use crate::plotutils::*;
 
 /// Plots in the terminal
 ///
 /// Removes all xs and ys that contain nan or infinity
 pub fn plot(xs: &[f32], ys: &[f32]) -> Result<(), Box<dyn Error>> {
-    let xsys = validate_input(xs,ys);
-
-    let (xmin,ymin): (f32,f32) = xsys.iter()
-                        .fold((0.,0.),|(xmin,ymin): (f32,f32),(x,y)| (xmin.min(*x),ymin.min(*y)));
-    let (xmax,ymax): (f32,f32) = xsys.iter()
-                        .fold((0.,0.),|(xmax,ymax): (f32,f32),(x,y)| (xmax.max(*x),ymax.max(*y)));
-    let xwidth = xmax-xmin;
-    let ywidth = ymax-ymin;
-    println!("xmin,xmax: {},{} ymin,ymax: {},{} xwidth,ywidth: {},{}",
-                        xmin,xmax,ymin,ymax,xwidth,ywidth);
-
     let leftmargin: usize = 12;
     let botmargin: usize = 3;
     let termwidth = usize::from(termsize::get().unwrap().cols);
     let termheight = usize::from(termsize::get().unwrap().rows);
 
-    let maxwidth = termwidth - leftmargin -1;
-    let maxheight = termheight - botmargin -1;
-    println!("maxwidth,maxheight: {},{}",maxwidth,maxheight);
-
-    let xtrans = |x: f32| maxwidth as f32/xwidth*(x-xmin);
-    let ytrans = |y: f32| maxheight as f32/ywidth*(y-ymin);
-    println!("{:>10} {:>10} {:>10} {:>10}","x","y","xtrans","ytrans");
-    for (x,y) in &xsys {
-        println!("{:10} {:10} {:10} {:10}",*x,*y,xtrans(*x),ytrans(*y));
-    }
-
-    let ybinwidth: f32 = ywidth/maxheight as f32;
-    let ybincenters: Vec<f32> = (0..maxheight+1)
-                        .map(|ibin| ymin + (ybinwidth*(ibin as f32+0.5)))
-                        .collect();
-    let data_ybins: Vec<Vec<usize>> = { // put mut var in its own scope to keep it here
-        let mut data_mut = vec![vec![];usize::try_from(maxheight+1).expect("n ybins fits in usize")];
-
-        for (x,y) in &xsys {
-            let xloc = xtrans(*x) as usize;
-            let yloc = ytrans(*y) as usize;
-            //println!("x,y: {},{} xloc,yloc: {},{}",*x,*y,xloc,yloc);
-            data_mut[yloc].push(xloc);
-        }
-        data_mut
+    let xsys = validate_input(xs,ys);
+    let axes = AxesMeta {
+        dataminmax: DataMinMax::find(&xsys),
+        termwidth: termwidth,
+        termheight: termheight,
+        leftmargin: leftmargin,
+        botmargin: botmargin,
     };
-    //for (ibin, yvals) in data_xbins.iter().enumerate() {
-    //    println!("{:10} {:?}",ibin,yvals);
-    //}
-    for (y,xvals) in zip(&ybincenters,&data_ybins).rev() {
-        let line: String = (0..maxwidth+1)
-                                .map(
-                                    |x| if xvals.iter().any(|matchx| *matchx == x) {
-                                            '●'
-                                        } else {
-                                            ' '
-                                        }
-                                )
-                                .collect();
-        println!("{:>10.3} │{}",y,line);
-    }
+
+    let plotteddata = drawdata(&xsys,&axes);
     //println!("{:10} ├{}┐","",(0..maxwidth).map(|x| if x == maxwidth / 2 {"┬"} else {"─"}).collect::<String>());
     //println!("{:10}  {:<10.3}{:>firsthalfwidth$.3}{xmax:>secondhalfwidth$.3}","",xmin,xmin + xwidth*0.5,firsthalfwidth=(maxwidth / 2)-10,xmax=xmax,secondhalfwidth=(maxwidth/2));
-    drawxaxis(xmin,xmax);
+    drawxaxis(&axes);
     Ok(())
 }
 
@@ -76,4 +34,35 @@ fn validate_input(xs: &[f32], ys: &[f32]) -> Vec<(f32,f32)> {
                                 .collect();
     xsys_buffer.sort_by(|(x1,_),(x2,_)| x1.partial_cmp(x2).unwrap());
     xsys_buffer
+}
+
+//////////////////////////////////////////////////////////////
+
+fn drawdata(xsys: &[(f32,f32)], axes: &AxesMeta) -> Vec<String> {
+    let mut result = vec!();
+
+    let ybincenters = axes.ybincenters();
+    let data_ybins: Vec<Vec<usize>> = { // put mut var in its own scope to keep it here
+        let mut data_mut = vec![vec![];axes.axesheight()];
+
+        for (x,y) in xsys {
+            let xloc = axes.xdatatoaxes(x);
+            let yloc = axes.ydatatoaxes(y);
+            data_mut[yloc].push(xloc);
+        }
+        data_mut
+    };
+    for (y,xvals) in zip(&ybincenters,&data_ybins).rev() {
+        let line: String = (0..axes.axeswidth()+1)
+                                .map(
+                                    |x| if xvals.iter().any(|matchx| *matchx == x) {
+                                            '●'
+                                        } else {
+                                            ' '
+                                        }
+                                )
+                                .collect();
+        result.push(format!("{:>10.3} │{}",y,line));
+    }
+    result
 }
--- src/plotutils.rs
+++ src/plotutils.rs
@@ -2,7 +2,9 @@
 const LEFTMARGIN: usize = 12;
 
 /// Draws the bottom x-axis line with a few ticks
-pub fn drawxaxis(xmin: f32,xmax: f32) -> () {
+pub fn drawxaxis(axesmeta: &AxesMeta) -> () {
+    let xmin = axesmeta.dataminmax.xmin;
+    let xmax = axesmeta.dataminmax.xmax;
     let xhalf = (xmax+xmin)*0.5;
     let axiswidth = termwidth() - LEFTMARGIN-1;
     let halfaxiswidth = axiswidth / 2;
@@ -14,3 +16,77 @@ pub fn drawxaxis(xmin: f32,xmax: f32) -> () {
 fn termwidth() -> usize {
     usize::from(termsize::get().unwrap().cols)
 }
+
+//////////////////////////////////////////////////////////////
+
+#[derive(Debug)]
+pub struct AxesMeta {
+    pub dataminmax: DataMinMax,
+    pub termwidth: usize,
+    pub termheight: usize,
+    pub leftmargin: usize,
+    pub botmargin: usize,
+}
+
+impl AxesMeta {
+    pub fn xdatatoaxes(&self, x: &f32) -> usize {
+        let resultfloat = self.axeswidth() as f32/self.dataminmax.xwidth()*(x-self.dataminmax.xmin);
+        resultfloat.floor() as usize
+    }
+    pub fn ydatatoaxes(&self, y: &f32) -> usize {
+        let resultfloat = self.axesheight() as f32/self.dataminmax.ywidth()*(y-self.dataminmax.ymin);
+        resultfloat.floor() as usize
+    }
+    pub fn axeswidth(&self) -> usize {
+        self.termwidth-self.leftmargin
+    }
+    pub fn axesheight(&self) -> usize {
+        self.termheight-self.botmargin
+    }
+    pub fn xbinwidth(&self) -> f32 {
+        self.dataminmax.xwidth()/self.axeswidth() as f32
+    }
+    pub fn ybinwidth(&self) -> f32 {
+        self.dataminmax.ywidth()/self.axesheight() as f32
+    }
+    pub fn xbincenters(&self) -> Vec<f32> {
+        (0..self.axeswidth()+1)
+            .map(|ibin| self.dataminmax.xmin + (self.xbinwidth()*(ibin as f32+0.5)))
+            .collect()
+    }
+    pub fn ybincenters(&self) -> Vec<f32> {
+        (0..self.axesheight()+1)
+            .map(|ibin| self.dataminmax.ymin + (self.ybinwidth()*(ibin as f32+0.5)))
+            .collect()
+    }
+}
+
+//////////////////////////////////////////////////////////////
+
+
+#[derive(Debug)]
+pub struct DataMinMax {
+    pub xmin: f32,
+    pub xmax: f32,
+    pub ymin: f32,
+    pub ymax: f32,
+}
+
+impl DataMinMax {
+    pub fn find(xsys: &[(f32,f32)]) -> DataMinMax {
+        let (xmin,ymin): (f32,f32) = xsys.iter()
+                            .fold((0.,0.),|(xmin,ymin): (f32,f32),(x,y)| (xmin.min(*x),ymin.min(*y)));
+        let (xmax,ymax): (f32,f32) = xsys.iter()
+                            .fold((0.,0.),|(xmax,ymax): (f32,f32),(x,y)| (xmax.max(*x),ymax.max(*y)));
+        DataMinMax { xmin: xmin, xmax: xmax, ymin: ymin, ymax: ymax }
+    }
+
+    pub fn xwidth(&self) -> f32 {
+        self.xmax-self.xmin
+    }
+
+    pub fn ywidth(&self) -> f32 {
+        self.ymax-self.ymin
+    }
+}
+
