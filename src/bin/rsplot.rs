b3eff38ee11a50f3fba33a8be5edabf394e70f28
Justin Hugon <jhugon@users.noreply.github.com>
2025-01-12 15:32:45 -0600

    rust fmt and remove a duplicated import (suggested by clippy)

--- src/bin/rsplot.rs
+++ src/bin/rsplot.rs
@@ -1,10 +1,9 @@
+use anyhow::anyhow;
 use clap::Parser;
 use learn_rust::plot;
 use patharg::InputArg;
-use std::error::Error;
-use anyhow::anyhow;
-use learn_rust::plot;
 use regex::Regex;
+use std::error::Error;
 
 /// Plots data from input in form x y
 ///
@@ -14,27 +13,34 @@ struct Arguments {
     infile: InputArg,
 }
 
-fn xyparseline(line: &str) -> Result<(f32,f32), Box<dyn Error>> {
+fn xyparseline(line: &str) -> Result<(f32, f32), Box<dyn Error>> {
     let re = Regex::new(r"^\s*(-??\d+(?:\.\d*)?)\s+(-??\d+(?:\.\d*)?)\s*$")?;
     let captureopt = re.captures(line);
     let (xnum, ynum) = if let Some(capture) = captureopt {
-        let (_, [xstr,ystr]) = capture.extract();
+        let (_, [xstr, ystr]) = capture.extract();
         (xstr.parse()?, ystr.parse()?)
     } else {
-        return Err(anyhow!("Couldn't parse line \"{}\", it should be two numbers seperated by whitespace",line).into());
+        return Err(anyhow!(
+            "Couldn't parse line \"{}\", it should be two numbers seperated by whitespace",
+            line
+        )
+        .into());
     };
-    Ok((xnum,ynum))
+    Ok((xnum, ynum))
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Arguments::parse();
     let infile = args.infile;
     for line in infile.lines()? {
-        let (_x,_y) = xyparseline(&line?)?;
+        let (_x, _y) = xyparseline(&line?)?;
     }
-    let xsys: Vec<(f32,f32)> = infile.lines()?.map(|line| xyparseline(&line?)).collect::<Result<Vec<(f32,f32)>,Box<dyn Error>>>()?;
-    let xs: Vec<f32>  = xsys.iter().map(|(x,_)| *x).collect();
-    let ys: Vec<f32> = xsys.iter().map(|(_,y)| *y).collect();
-    plot(&xs,&ys)?;
+    let xsys: Vec<(f32, f32)> = infile
+        .lines()?
+        .map(|line| xyparseline(&line?))
+        .collect::<Result<Vec<(f32, f32)>, Box<dyn Error>>>()?;
+    let xs: Vec<f32> = xsys.iter().map(|(x, _)| *x).collect();
+    let ys: Vec<f32> = xsys.iter().map(|(_, y)| *y).collect();
+    plot(&xs, &ys)?;
     Ok(())
 }
